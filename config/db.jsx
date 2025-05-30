1af4d769e3bd8fbcff17f5600be559bd2e9428b1
Dhruv <dhruvkashyap458@gmail.com>
2025-05-30 14:31:31 +0530

    added schema for the courses

--- config/db.jsx
+++ config/db.jsx
@@ -3,3 +3,5 @@ import { drizzle } from 'drizzle-orm/neon-http';
 
 const pg = neon(process.env.DATABASE_URL);
 export const db = drizzle({ client: pg });
+
+
--- config/schema.ts
+++ config/schema.ts
@@ -1,8 +1,21 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, json, pgTable, varchar, boolean, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
- subscriptionId: varchar({ length: 255 }),
+  subscriptionId: varchar({ length: 255 }),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const coursesTable = pgTable("courses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  cid: varchar({ length: 255 }).notNull().unique(),
+  name: varchar({ length: 255 }).notNull(),
+  description: text(),
+  noOfChapters: integer(),
+  includeVideo: boolean().default(false),
+  level: varchar({ length: 50 }),
+  category: varchar({ length: 255 }),
+  userEmail: varchar('user_email', { length: 255 }).notNull().references(() => usersTable.email),
+  courseJson: json(),
 });
